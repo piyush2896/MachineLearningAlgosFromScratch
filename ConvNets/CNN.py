228e56cb0b4637a5e294908a82d6d97f2e3d0010
Piyush Malhotra <piyushmalhotra28@gmail.com>
2017-12-06 13:26:50 +0530

    Refactor Code

--- ConvNets/CNN.py
+++ ConvNets/CNN.py
@@ -3,6 +3,43 @@ import matplotlib.pyplot as plt
 from math import ceil, floor
 
 
+def _calc_pad(X, W, strides):
+    in_height = X.shape[1]
+    in_width = X.shape[2]
+    filter_height = W.shape[0]
+    filter_width = W.shape[1]
+
+    pad_along_height = (in_height * (strides[0] - 1) -
+                        strides[0] + filter_height) / 2
+    pad_along_height = max(pad_along_height, 0)
+    pad_along_width = (in_width * (strides[1] - 1) -
+                       strides[1] + filter_width) / 2
+    pad_along_width = max(pad_along_width, 0)
+
+    return pad_along_height, pad_along_width
+
+
+def _apply_padding(X, W, strides):
+    (m, n_H_prev, n_W_prev, n_C) = X.shape
+    pad_along_height, pad_along_width = _calc_pad(X, W, strides)
+    n_H = n_H_prev + 2 * pad_along_height
+    n_W = n_H_prev + 2 * pad_along_width
+    X_pad = np.pad(X, ((0, 0), (ceil(pad_along_height), floor(pad_along_height)),
+                       (ceil(pad_along_width), floor(pad_along_width)), (0, 0)),
+                   'constant', constant_values=(0, 0))
+    return X_pad, pad_along_height, pad_along_width
+
+
+def _output_shape(X, W, pads=[2, 2], strides=[1, 1]):
+    (m, in_height, in_width, n_C) = X.shape
+    (f_height, f_width, n_C_prev, n_C) = W.shape
+    out_height = int(1 + (in_height + 2 * pads[0] -
+                          f_height) / strides[0])
+    out_width = int(1 + (in_width + 2 * pads[1] -
+                         f_width) / strides[1])
+    return (m, out_height, out_width, n_C)
+
+
 class Conv2d(object):
 
     def __init__(self, W, strides=(1, 1), pad='VALID'):
@@ -10,32 +47,6 @@ class Conv2d(object):
         self.strides = strides
         self.pad = pad
         self.b = np.random.randn(1, 1, 1, self.W.shape[3])
-
-    def _calc_pad(self, X):
-        in_height = X.shape[1]
-        in_width = X.shape[2]
-        filter_height = self.W.shape[0]
-        filter_width = self.W.shape[1]
-
-        pad_along_height = (in_height * (self.strides[0] - 1) -
-                            self.strides[0] + filter_height) / 2
-        self.pad_along_height = max(pad_along_height, 0)
-        pad_along_width = (in_width * (self.strides[1] - 1) -
-                           self.strides[1] + filter_width) / 2
-        self.pad_along_width = max(pad_along_width, 0)
-
-        return self.pad_along_height, self.pad_along_width
-
-    def _apply_padding(self, X):
-        (m, n_H_prev, n_W_prev, n_C) = X.shape
-        pad_along_height, pad_along_width = self._calc_pad(X)
-        print(pad_along_height, pad_along_width)
-        n_H = n_H_prev + 2 * pad_along_height
-        n_W = n_H_prev + 2 * pad_along_width
-        self.X_pad = np.pad(X, ((0, 0), (ceil(pad_along_height), floor(pad_along_height)),
-                                (ceil(pad_along_width), floor(pad_along_width)), (0, 0)),
-                            'constant', constant_values=(0, 0))
-        return self.X_pad
 
     def _output_shape(self):
         (m, in_height, in_width, n_C) = self.X.shape
@@ -54,12 +65,13 @@ class Conv2d(object):
         if self.pad == 'VALID':
             self.X_pad = self.X
         elif self.pad == 'SAME':
-            self.X_pad = self._apply_padding(self.X)
+            (self.X_pad, self.pad_along_height, self.pad_along_width) = \
+                                    _apply_padding(self.X, self.W, self.strides)
 
         (m, in_height, in_width, n_C) = self.X.shape
         (f_height, f_width, n_C_prev, n_C) = self.W.shape
 
-        out_shape = self._output_shape()
+        out_shape = _output_shape(X, W, [self.pad_along_height, self.pad_along_width], self.strides)
         (m, out_height, out_width, n_C) = out_shape
         self.Z = np.zeros(out_shape)
 
@@ -83,10 +95,10 @@ class Conv2d(object):
 
 
 if __name__ == '__main__':
-    conv = Conv2d(np.random.randn(3, 3, 2, 10))
+    w = np.random.randn(3, 3, 2, 10)
     np.random.seed(1)
     x = np.random.randn(4, 3, 3, 2)
-    x_pad = conv._apply_padding(x)
+    x_pad, pad_along_height, pad_along_width = _apply_padding(x, w, strides=[1, 1])
     print ("x.shape =", x.shape)
     print ("x_pad.shape =", x_pad.shape)
     print ("x[1,1] =", x[1,1])
